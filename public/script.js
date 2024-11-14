88cd168dbebfc19b2cb88c61c83751f7deff5b9b
thatguys2good <da0ro1jecu@hotmail.com>
2024-11-14 17:32:41 -0500

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,5 +1,5 @@
-
 const correctKey = "beta";
+
 function showNotification(message, isSuccess) {
   const notification = document.getElementById("notification");
   notification.textContent = message;
@@ -12,19 +12,40 @@ function showNotification(message, isSuccess) {
     notification.classList.add("error");
     notification.classList.remove("success");
   }
+
   setTimeout(() => {
     notification.classList.remove("show");
   }, 2000);
 }
+
+function handleKey(event) {
+  if (event.key === "Enter") {
+    checkKey();
+  }
+}
+
 function checkKey() {
   const keyInput = document.getElementById("keyInput").value;
+
   if (keyInput === correctKey) {
     showNotification("Correct key entered!", true);
-    document.getElementById("playButton").classList.remove("hidden");
+    setTimeout(() => {
+      document.querySelector(".container").style.display = "none";
+      displayPlayButton();
+    }, 1000);
   } else {
     showNotification("Wrong key, try again!", false);
   }
 }
+
+function displayPlayButton() {
+  const playButton = document.createElement("div");
+  playButton.classList.add("play-button");
+  playButton.innerHTML = `<button onclick="startGame()">Play</button>`;
+  document.body.appendChild(playButton);
+  playButton.style.display = "flex";
+}
+
 function startGame() {
-  alert("thanks!");
+  alert("fein");
 }
